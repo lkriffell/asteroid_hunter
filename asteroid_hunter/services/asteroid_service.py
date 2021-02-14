a731f749de59a929334ba117f3d944927315c079
Logan Riffell <lkriffell@gmail.com>
2021-02-13 21:09:24 -0700

    Refactor for readability and add close_approach_by_month

--- asteroid_hunter/services/asteroid_service.py
+++ asteroid_hunter/services/asteroid_service.py
@@ -5,11 +5,16 @@ import requests
 class AsteroidService:
   def conn(uri):
     neo_key = LocalApiKeys().neo_key
-    return 'https://api.nasa.gov/neo/rest/v1/neo/' + uri + neo_key
+    return 'https://api.nasa.gov/neo/rest/v1/' + uri + neo_key
 
   def browse_neos(page):
     params = {'page': page}
-    return requests.get(AsteroidService.conn('browse?'), params=params)
+    path = AsteroidService.conn('neo/browse?')
+    return requests.get(path, params=params)
+
+  def close_approach_by_month():
+    path = AsteroidService.conn('feed?start_date=2021-01-01&end_date=2021-01-08')
+    requests.get(path, params=params)
 
   def asteroid_by_id(id):
     return requests.get(AsteroidService.conn(id + '?'))
