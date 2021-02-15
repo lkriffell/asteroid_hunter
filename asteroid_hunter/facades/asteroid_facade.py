59c66ae0f3b52714a72de9a1b6d7ba4b8b7f08d5
Logan Riffell <lkriffell@gmail.com>
2021-02-15 15:10:25 -0700

    Add code for closest approaches in a month with testing

--- asteroid_hunter/facades/asteroid_facade.py
+++ asteroid_hunter/facades/asteroid_facade.py
@@ -1,32 +1,87 @@
 from asteroid_hunter.services.asteroid_service import AsteroidService
 import json
+from pprint import pprint
+import datetime
 
 class AsteroidFacade:
   def closest_approach_id():
     page = 0
-    approach_data = AsteroidFacade.get_approach_data_by_page(page)
-    closest_approach = approach_data[0]
-    # while approach_data != []:
+    asteroid_data = AsteroidFacade.get_asteroid_data_by_page(page)
+    closest_approach = asteroid_data[0]
+    # while asteroid_data != []:
     while page < 230:
-      approach_data = AsteroidFacade.get_approach_data_by_page(page)
+      asteroid_data = AsteroidFacade.get_asteroid_data_by_page(page)
       page += 1
-      for asteroid in approach_data:
-        closest_approach = AsteroidFacade.consider_all_approaches(asteroid, closest_approach)
+      for asteroid in asteroid_data:
+        closest_approach = AsteroidFacade.look_for_closer_approach(asteroid, closest_approach)
     return closest_approach['id']
 
-  
-  def asteroid_by_id(id):
-    return AsteroidService.asteroid_by_id(id).json()
-
-  # helpers
-  
-  def get_approach_data_by_page(page):
+  # helpers for closest_approach_id()
+  def get_asteroid_data_by_page(page):
     return AsteroidService.browse_neos(page).json()['near_earth_objects']
 
-  def consider_all_approaches(asteroid, closest_approach):
-    # Changes the closest approach if any of this asteroids approache's were closer
+  def look_for_closer_approach(asteroid, closest_approach):
+    # Changes the closest approach if any of this asteroid's approaches were closer
     for close_approach in asteroid['close_approach_data']:
       if (close_approach != []
           and close_approach['miss_distance']['astronomical'] < closest_approach['close_approach_data'][0]['miss_distance']['astronomical']):
         closest_approach = asteroid
     return closest_approach
+  # --------------------------------
+
+  def close_approaches_by_month(result_size, year, month):
+    finished = False
+    day = 1
+    calendar = AsteroidFacade.calendar(year)
+    closest_approaches = []
+    while finished == False:
+      start_date = datetime.date(year, month, day)
+      day = AsteroidFacade.add_a_week_to_day(month, day, calendar)
+      end_date = datetime.date(year, month, day)
+      asteroid_data = AsteroidFacade.get_asteroid_data_by_date(start_date, end_date)
+      AsteroidFacade.find_and_sort_closest_approaches(asteroid_data, closest_approaches, start_date)
+      if day == calendar[month] : finished = True
+    return closest_approaches[0 : result_size]
+
+  # helpers for close_approaches_by_month()
+  def get_asteroid_data_by_date(start_date, end_date):
+    return AsteroidService.asteroid_approaches_by_week(start_date, end_date).json()['near_earth_objects']
+
+  def find_and_sort_closest_approaches(asteroid_data, closest_approaches, date):
+    for day in range (0, 7):
+      for asteroid in asteroid_data[date.strftime("%Y-%m-%d")]:
+        closest_approaches.append(asteroid)
+        closest_approaches.sort(key=lambda x: x['close_approach_data'][0]['miss_distance']['astronomical'])
+        # This next line would work properly but when I try to return the final list it would cut the list to 5 elements no matter what
+        # closest_approaches = closest_approaches[0 : result_size]
+      date += datetime.timedelta(1)
+    return closest_approaches
+
+  def calendar(year):
+    calendar = {
+                1: 31,
+                2: 28,
+                3: 31,
+                4: 30,
+                5: 31,
+                5: 31,
+                6: 30,
+                7: 31,
+                8: 31,
+                9: 30,
+                10: 31,
+                11: 30,
+                12: 31,
+                }
+    if (year % 4) == 0 : calendar[2] = 29
+    return calendar
+
+  def add_a_week_to_day(month, day, calendar):
+    if (day + 6) > calendar[month]:
+      return calendar[month]
+    else:
+      return day + 6
+  #----------------------------------------
+  
+  def asteroid_by_id(id):
+    return AsteroidService.asteroid_by_id(id)
--- tests/test_asteroid_facade.py
+++ tests/test_asteroid_facade.py
@@ -8,11 +8,23 @@ def test_closest_approach_id():
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/browse.yaml', record_mode='once')
 def test_get_approach_data_by_page():
-  approach_data = AsteroidFacade.get_approach_data_by_page(0)
+  approach_data = AsteroidFacade.get_asteroid_data_by_page(0)
   assert len(approach_data) == 20
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/asteroid_by_id.yaml', record_mode='once')
 def test_asteroid_by_id():
-  asteroid = AsteroidFacade.asteroid_by_id('3292020')
+  asteroid = AsteroidFacade.asteroid_by_id('3292020').json()
   assert asteroid['name'] == '(2005 TF)'
+
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/asteroid_approaches_by_month.yaml', record_mode='once')
+def test_close_approaches_by_month_can_get_top_ten():
+  closest_approaches_in_january = AsteroidFacade.close_approaches_by_month(10, 2021, 1)
+  assert len(closest_approaches_in_january) == 10
+  assert closest_approaches_in_january[0]['name'] == '(2021 BO)'
+
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/asteroid_approaches_by_month.yaml', record_mode='once')
+def test_close_approaches_by_month_can_get_top_twenty():
+  closest_approaches_in_january = AsteroidFacade.close_approaches_by_month(20, 2021, 1)
+  assert len(closest_approaches_in_january) == 20
+  assert closest_approaches_in_january[0]['name'] == '(2021 BO)'
 
