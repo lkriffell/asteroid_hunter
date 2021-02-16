3dcc1eeb9c55dd287b34b7d973ea5c0e00dd53fa
Logan Riffell <lkriffell@gmail.com>
2021-02-16 12:52:29 -0700

    Update to assert type

--- tests/test_asteroid_facade.py
+++ tests/test_asteroid_facade.py
@@ -3,28 +3,28 @@ import vcr
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/browse.yaml', record_mode='once')
 def test_nearest_misses():
-  ten_nearest_misses = AsteroidFacade.nearest_misses()
+  ten_nearest_misses = AsteroidFacade.nearest_misses(10)
   assert len(ten_nearest_misses) == 10
   for near_miss in ten_nearest_misses:
     assert type(near_miss['close_approach_data']) == dict
 
-# @vcr.use_cassette('tests/fixtures/vcr_cassettes/nearest_miss.yaml', record_mode='once')
-# def test_nearest_misses_can_return_the_closest_miss():
-#   nearest_miss = AsteroidFacade.nearest_misses(1)
-#   assert len(ten_nearest_misses) == 1
-#   assert type(nearest_miss['close_approach_data']) == dict
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/nearest_miss.yaml', record_mode='once')
+def test_nearest_misses_can_return_the_closest_miss():
+  nearest_miss = AsteroidFacade.nearest_misses(1)
+  assert type(nearest_miss) == dict
+  assert type(nearest_miss['close_approach_data']) == dict
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/january_approaches_by_month.yaml', record_mode='once')
 def test_close_approaches_by_month_can_get_top_ten():
   closest_approaches_in_january = AsteroidFacade.close_approaches_by_month(10, 2021, 1)
   assert len(closest_approaches_in_january['closest_approaches']) == 10
   assert closest_approaches_in_january['closest_approaches'][0]['name'] == '(2021 BO)'
-  assert closest_approaches_in_january['element_count'] == 481
+  assert closest_approaches_in_january['element_count'] == 483
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/january_approaches_by_month.yaml', record_mode='once')
 def test_close_approaches_by_month_can_get_top_twenty():
   closest_approaches_in_january = AsteroidFacade.close_approaches_by_month(20, 2021, 1)
   assert len(closest_approaches_in_january['closest_approaches']) == 20
   assert closest_approaches_in_january['closest_approaches'][0]['name'] == '(2021 BO)'
-  assert closest_approaches_in_january['element_count'] == 481
+  assert closest_approaches_in_january['element_count'] == 483
 
