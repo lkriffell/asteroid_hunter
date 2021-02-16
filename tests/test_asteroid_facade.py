f62c62501d1c2182cc860983678f926c845af6fc
Logan Riffell <lkriffell@gmail.com>
2021-02-15 19:41:45 -0700

    Add tests for nearest misses

--- tests/test_asteroid_facade.py
+++ tests/test_asteroid_facade.py
@@ -2,19 +2,17 @@ from asteroid_hunter.facades.asteroid_facade import AsteroidFacade
 import vcr
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/browse.yaml', record_mode='once')
-def test_closest_approach_id():
-  closest_approach_id = AsteroidFacade.closest_approach_id()
-  assert closest_approach_id == '3292020'
+def test_nearest_misses():
+  ten_nearest_misses = AsteroidFacade.nearest_misses()
+  assert len(ten_nearest_misses) == 10
+  for near_miss in ten_nearest_misses:
+    assert type(near_miss['close_approach_data']) == dict
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/browse.yaml', record_mode='once')
-def test_get_approach_data_by_page():
-  approach_data = AsteroidFacade.get_asteroid_data_by_page(0)
-  assert len(approach_data) == 20
-
-@vcr.use_cassette('tests/fixtures/vcr_cassettes/asteroid_by_id.yaml', record_mode='once')
-def test_asteroid_by_id():
-  asteroid = AsteroidFacade.asteroid_by_id('3292020').json()
-  assert asteroid['name'] == '(2005 TF)'
+def test_nearest_misses_can_return_the_closest_miss():
+  nearest_miss = AsteroidFacade.nearest_misses(1)
+  assert len(ten_nearest_misses) == 1
+  assert type(nearest_miss['close_approach_data']) == dict
 
 @vcr.use_cassette('tests/fixtures/vcr_cassettes/january_approaches_by_month.yaml', record_mode='once')
 def test_close_approaches_by_month_can_get_top_ten():
